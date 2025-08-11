9027745d2deb817a797535f3bd4957d0d2558630
hayk2377 <haykelmuktar@gmail.com>
2025-08-11 08:55:27 +0300

    feat: deployment on render

--- backend/triproute/settings.py
+++ backend/triproute/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trip-planner-pi60.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
